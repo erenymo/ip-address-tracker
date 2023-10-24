b0cf0f4ce02f44f51397d48eab9b0f2007f3a84e
Eren <erenbalta342115@gmail.com>
2023-10-24 11:26:29 +0300

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,2 @@
-export const API_KEY = "at_BIDmYVQzrVmn5HzxSAu3sXvF923wo";
+export const API_KEY = "at_rDTD1SSL0UoUjymbUcUC2WAotcpYY";
 export const API_URL = "https://geo.ipify.org/api/v2/country,city?apiKey=";
